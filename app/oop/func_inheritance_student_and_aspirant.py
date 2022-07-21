8c92722ccbc5b94027d0c6718d9cc99169ea7908
myralitv <m.litvinenko2012@gmail.com>
2022-07-21 23:15:40 +0300

    Added changes for functions from OOP module.

--- app/oop/func_inheritance_student_and_aspirant.py
+++ app/oop/func_inheritance_student_and_aspirant.py
@@ -1,47 +1,53 @@
 class Student:
-    firstName = None
-    lastName = None
+    first_name = None
+    last_name = None
     group = None
-    averageMark = None
-    scientificWork = None
+    average_mark = None
 
-    def __init__(self, firstName: str, lastName: str, group: str, scientificWork: int):
-        self.firstName = firstName
-        self.lastName = lastName
+    def __init__(self, first_name: str, last_name: str, group: str, average_mark: int):
+        self.firstName = first_name
+        self.lastName = last_name
         self.group = group
-        self.scientificWork = scientificWork
+        self.average_mark = average_mark
 
-    def if_student(self):
-        return self.scientificWork < 1
-
-    def getScholarship(self, marks: tuple):
-        if self.if_student():
-            self.averageMark = sum(marks) / len(marks)
-            if self.averageMark == 5:
-                return '100 гривень'
-            else:
-                return '80 гривень'
+    def get_scholarship(self):
+        if self.average_mark == 5:
+            return '100 гривень'
         else:
-            return f'{self.firstName} {self.lastName} is Aspirant'
+            return '80 гривень'
 
 
 class Aspirant(Student):
-    def if_aspirant(self):
-        return self.scientificWork >= 1
+    scientific_work = "Title"
 
-    def getScholarship(self, marks: tuple):
-        if self.if_aspirant():
-            self.averageMark = sum(marks) / len(marks)
-            if self.averageMark == 5:
-                return '200 гривень'
-            else:
-                return '180 гривень'
+    def __init__(self, first_name: str, last_name: str, group: str, average_mark: int, scientific_work: str):
+        Student.__init__(self, first_name, last_name, group, average_mark)
+        self.scientific_work = scientific_work
+
+    def set_scientific_work(self):
+        return self.scientific_work
+
+    def get_scholarship(self):
+        if self.average_mark == 5:
+            return '200 гривень'
         else:
-            return f'{self.firstName} {self.lastName} isn\'t Aspirant'
+            return '180 гривень'
 
 
 if __name__ == '__main__':
-    student = Student('Myroslav', 'Lytvynenko', '1', 0)
-    print(student.getScholarship((5, 5, 5, 4)))
-    aspirant = Aspirant('Nikita', 'Yarmolenko', '2', 1)
-    print(aspirant.getScholarship((5, 5, 5, 5)))
+    student = Student('Myroslav', 'Lytvynenko', '1', 4)
+    print(student.get_scholarship())
+    aspirant = Aspirant('Nikita', 'Yarmolenko', '2', 5, "Number1")
+    print(aspirant.get_scholarship())
+
+    object_students_aspirants = []
+
+    object_students_aspirants.append(Student('Myroslav', 'Lytvynenko', '1', 4))
+    object_students_aspirants.append(Aspirant('Nikita', 'Yarmolenko', '2', 5, "Number1"))
+    object_students_aspirants.append(Student('Vlad', 'Bereka', '2', 5))
+    object_students_aspirants.append(Aspirant('Alex', 'Yermolenko', '1', 4, "Number2"))
+    object_students_aspirants.append(Student('Dima', 'Kaminskii', '2', 2))
+    object_students_aspirants.append(Aspirant('Vova', 'Kondratenko', '1', 3, "Number3"))
+
+    for human in object_students_aspirants:
+        print(human.get_scholarship())
